784fa6cdd0208362a4497e0aa3ad061117da4b44
itcast-hsian <407775611@qq.com>
2020-03-24 16:00:34 +0800

    新建一个登陆页

--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,19 @@ Vue.use(VueRouter)
 
 // 路由配置
 const routes = [
-  
+	{
+		path: "/login",
+		// 除了首页之外的页面都可以用懒加载,@符号代表的src这个目录
+		// .vue这个扩展名可以忽略
+		component: () => import("@/views/Login")
+	}
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  //路由配置
-  routes
+	mode: 'history',
+	base: process.env.BASE_URL,
+	//路由配置
+	routes
 })
 
 export default router
