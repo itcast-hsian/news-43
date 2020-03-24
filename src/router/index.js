be57222422a8c8323918f8b95090c2a33afc95c8
itcast-hsian <407775611@qq.com>
2020-03-24 15:41:05 +0800

    清理项目初始化时候的文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,12 @@
 import Vue from 'vue'
 // 导入路由
 import VueRouter from 'vue-router'
-// 导入组件，home是一个普通页面组件，home是首页
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+// 路由配置
 const routes = [
-  {
-    path: '/',
-    component: Home
-  },
-  {
-    path: '/about',
-    // 组件的懒加载，只有当我访问这个页面的时候，才会加载这个组件的资源
-    component: () => import('../views/About.vue')
-  }
+  
 ]
 
 const router = new VueRouter({
