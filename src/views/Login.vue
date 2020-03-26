2bd7faeaf6e48b9b0d6c9a9db3cec5cb047ad056
itcast-hsian <407775611@qq.com>
2020-03-26 14:39:10 +0800

    把vant的表单组件引入到登录页

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,18 +10,33 @@
             <span class="iconfont iconnew"></span>
         </div>
 
-        <!-- 表单 -->
-        <div class="form">
-            <div class="form-item">
-                <input placeholder="手机号码" v-model="form.username">
+        <!-- 使用vant的表单 -->
+        <!-- van-form是表单的组件， @submit是表单按钮提交的事件 -->
+        <van-form @submit="onSubmit">
+            <!-- van-field是表单的字段 -->
+            <!-- rules是表单字段的规则，required表示这个输入框是必填 -->
+            <van-field
+                v-model="form.username"
+                name="手机号码"
+                placeholder="手机号码"
+                :rules="[{ required: true, message: '请填写手机号码' }]"
+            />
+            <!-- 密码输入框，和上面的属性是一样的 -->
+            <van-field
+                v-model="form.password"
+                type="password"
+                name="密码"
+                placeholder="密码"
+                :rules="[{ required: true, message: '请填写密码' }]"
+            />
+            <div>
+                <!-- 如果这个按钮是在van-form组件内部，
+                并且按钮的native-type="submit"，说明点击这个按钮就会触发submit事件 -->
+                <van-button round block type="info" native-type="submit">
+                    登录
+                </van-button>
             </div>
-            <div class="form-item">
-                <input type="password" placeholder="请输入密码" v-model="form.password">
-            </div>
-            <div class="form-item">
-                <button @click="handleClick">登录</button>
-            </div>
-        </div>
+        </van-form>
     </div>
 </template>
 
@@ -38,25 +53,31 @@ export default {
     },
     methods: {
         // 点击登录按钮的事件
-        handleClick(){
-            // 调用axios发起异步请求，类似$.ajax(类似不代表一样)
-            this.$axios({
-                // 接口地址
-                url: "http://127.0.0.1:3000/login",
-                // 声明请求的方法为post请求(一定要注册这个method没有s)
-                // 跟vue的methods属性毫无关系
-                method: "POST",
-                // 参数
-                data: this.form
-                // .then方法里面的函数就是成功的回调函数,axios没有succces
-            }).then(res => {
-                // 获取到返回的信息
-                const {message} = res.data;
-                // 使用vant的弹窗提示用，success表示成功的弹窗
-                this.$toast.success(message);
-            })
+        // handleClick(){
+        //     // 调用axios发起异步请求，类似$.ajax(类似不代表一样)
+        //     this.$axios({
+        //         // 接口地址
+        //         url: "http://127.0.0.1:3000/login",
+        //         // 声明请求的方法为post请求(一定要注册这个method没有s)
+        //         // 跟vue的methods属性毫无关系
+        //         method: "POST",
+        //         // 参数
+        //         data: this.form
+        //         // .then方法里面的函数就是成功的回调函数,axios没有succces
+        //     }).then(res => {
+        //         // 获取到返回的信息
+        //         const {message} = res.data;
+        //         // 使用vant的弹窗提示用，success表示成功的弹窗
+        //         this.$toast.success(message);
+        //     })
 
-        }
+        // },
+
+        // 提交表单时候触发的事件，该事件通过校验才会触发
+        // values是表单返回的值，这里的values我们用不上，数据可以在this.form里面拿
+        onSubmit(values) {
+            console.log('submit', this.form);
+        },
     }
 };
 </script>
