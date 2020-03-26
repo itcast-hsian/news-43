52c722d151ebb5d3891447d91989408aadfdde32
itcast-hsian <407775611@qq.com>
2020-03-26 10:21:47 +0800

    登录页布局样式

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -31,6 +31,51 @@ export default {
 };
 </script>
 
-<style>
+<style lang="less">
+    .container{
+        padding: 20 / 360 * 100vw;
+    }
 
+    .back-btn{
+        font-size: 27 / 360 * 100vw;
+        line-height: 0;
+    }
+
+    .logo{
+        text-align: center;
+        margin-top: 20 / 360 * 100vw;
+    }
+
+    .logo span{
+        font-size: 126 / 360 * 100vw;
+        color: #cc3300;
+    }
+    
+    .form-item{
+         margin-top: 20 / 360 * 100vw;
+    }
+
+    .form-item input{
+        width: 100%;
+        height: 44 / 360 * 100vw;
+        box-sizing: border-box;
+        line-height: 44 / 360 * 100vw;
+        background: none;
+        border:none;
+        border-bottom: 1px #999 solid;
+        outline: none; // 输入框点击时候没有高亮状态
+        font-size: 16px; // 文字大小尽量用像素，因为浏览器最低支持12px
+    }
+
+    .form-item button{
+        margin-top: 50 / 360 * 100vw;
+        width: 100%;
+        height: 48 / 360 * 100vw;
+        background: #cc3300;
+        color: #fff;
+        border-radius: 50px;
+        border: none;
+        outline: none;
+        font-size: 16px;
+    }
 </style>
