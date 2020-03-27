9ad82e97a6d3e8c1cdc1b44f24f137007f63ccd0
itcast-hsian <407775611@qq.com>
2020-03-27 09:41:41 +0800

    个人中心页头部

--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -1,13 +1,54 @@
 <template>
-  <div>个人中心</div>
+    <div class="container">
+		<!-- 头部 -->
+		<div class="header">
+			<!-- 头像 -->
+			<div class="avatar">
+				<img src="https://www.baidu.com/img/bd_logo1.png" alt="">
+			</div>
+			<!-- 姓名 -->
+			<div class="profile">
+				<div>
+					<span class="iconfont iconxingbienan"></span>
+					火星网友
+				</div>
+				<p>2020-03-27</p>
+			</div>
+			<!-- 右侧的箭头图标 -->
+			<span class="arrow iconfont iconjiantou1"></span>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped lang="less">
+.header{
+	padding: 20 / 360 * 100vw;
+	display: flex;
+	justify-content: space-between;
+	align-items:center;
+	border-bottom: 5px #eee solid;
 
+	.avatar{
+		img{
+			display: block;
+			width:70 / 360 * 100vw;
+			height: 70 / 360 * 100vw;
+			border-radius: 50%;
+			object-fit: contain;
+		}
+	}
+
+	.profile{
+		flex:1;
+		padding-left: 20 / 360 * 100vw;
+		line-height: 1.5;
+		p{
+			color: #999;
+		}
+	}
+}
 </style>
