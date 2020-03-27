4c3fea3bc91ef3970b5d7cdce965b79383c6828a
itcast-hsian <407775611@qq.com>
2020-03-27 14:51:32 +0800

    添加顶部的导航条

--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="container">
+		<!-- 顶部的导航条 -->
+		<div class="navigate-bar">
+			<!-- $router.back()是实例下的属性，可以直接在模板中渲染 -->
+			<span class="iconfont iconjiantou2" @click="$router.back()"></span>
+			<strong>个人中心</strong>
+			<!-- $router.push()是实例下的属性，可以直接在模板中渲染 -->
+			<span class="iconfont iconshouye" @click="$router.push('/')"></span>
+		</div>
+
 		<!-- 头部 -->
 		<div class="header">
 			<!-- 头像 -->
@@ -87,6 +96,19 @@ export default {
 </script>
 
 <style scoped lang="less">
+.navigate-bar{
+	line-height: 48 / 360 * 100vw;
+	display: flex;
+	justify-content: space-between;
+	align-items:center;
+	padding: 0 20 / 360 * 100vw;
+	border-bottom:1px #eee solid;
+
+	.iconshouye{
+		font-size: 20px;
+	}
+}
+
 .header{
 	padding: 20 / 360 * 100vw;
 	display: flex;
