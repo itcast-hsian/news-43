93d575dcff3fd3bc8825cba24f03db70abdb4323
itcast-hsian <407775611@qq.com>
2020-03-30 16:01:41 +0800

    我的收藏页面的数据渲染

--- src/views/Star.vue
+++ src/views/Star.vue
@@ -3,24 +3,26 @@
         <!-- 头部组件 -->
         <NavigateBar title="我的收藏"/>
 
-        <!-- 循环的结构,少于3张图片的布局 -->
-        <div class="star-item imgText">
-            <div class="imgText-left">
-                <h4>林志玲穿透视黑纱裙米兰看秀腹部微隆显孕味</h4>
-                <p>火星时报    52跟帖</p>
+        <div v-for="(item, index) in stars" :key="index">
+            <!-- 循环的结构,少于3张图片的布局 -->
+            <div class="imgText" v-if="item.cover.length < 3 && item.cover.length > 0">
+                <div class="imgText-left">
+                    <h4>{{item.title}}</h4>
+                    <p>{{item.user.nickname}}  {{item.comments.length}}跟帖</p>
+                </div>
+                <img :src="$axios.defaults.baseURL + item.cover[0].url">
             </div>
-            <img src="https://www.baidu.com/img/bd_logo1.png">
-        </div>
 
-        <!-- 循环的结构,大于3张图片的布局 -->
-        <div class="imgList">
-            <h4>林志玲穿透视黑纱裙米兰看秀腹部微隆显孕味</h4>
-            <div class="images">
-                <img src="https://www.baidu.com/img/bd_logo1.png">
-                <img src="https://www.baidu.com/img/bd_logo1.png">
-                <img src="https://www.baidu.com/img/bd_logo1.png">
-            </div>
-            <p>火星时报  52跟帖</p>
+            <!-- 循环的结构,大于3张图片的布局 -->
+            <!-- <div class="imgList">
+                <h4>林志玲穿透视黑纱裙米兰看秀腹部微隆显孕味</h4>
+                <div class="images">
+                    <img src="https://www.baidu.com/img/bd_logo1.png">
+                    <img src="https://www.baidu.com/img/bd_logo1.png">
+                    <img src="https://www.baidu.com/img/bd_logo1.png">
+                </div>
+                <p>火星时报  52跟帖</p>
+            </div> -->
         </div>
     </div>
 </template>
@@ -30,8 +32,33 @@
 import NavigateBar from "@/components/NavigateBar"
 
 export default {
+    data(){
+        return {
+            // 我的收藏文章列表 
+            stars: []
+        }
+    },
     components: {
         NavigateBar
+    },
+
+    mounted(){
+        // 本地的数据
+        const localUserJson = JSON.parse(localStorage.getItem("userInfo"));
+
+        // 请求我的收藏数据
+        this.$axios({
+            url: "/user_star",
+            // 添加头信息
+			headers: {
+				Authorization: localUserJson.token
+			}
+        }).then(res => {
+            // 我的收藏文章列表
+            const {data} = res.data;
+            // 把列表数据保存到data
+            this.stars = data;
+        })
     }
 };
 </script>
@@ -61,11 +88,12 @@ export default {
     }
 
     img{
-        width: 121/360*100vw;
+        width: 110/360*100vw;
         height: 75/360*100vw;
         display: block;
         object-fit: cover;
         margin-left: 10/360*100vw;
+        flex-shrink: 0;
     }
 }
 
