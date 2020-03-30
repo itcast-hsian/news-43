0882ee0ffee0ebba055e65fdd73c647222e6bd81
itcast-hsian <407775611@qq.com>
2020-03-30 15:45:06 +0800

    我的收藏页的静态布局

--- src/views/Star.vue
+++ src/views/Star.vue
@@ -11,6 +11,17 @@
             </div>
             <img src="https://www.baidu.com/img/bd_logo1.png">
         </div>
+
+        <!-- 循环的结构,大于3张图片的布局 -->
+        <div class="imgList">
+            <h4>林志玲穿透视黑纱裙米兰看秀腹部微隆显孕味</h4>
+            <div class="images">
+                <img src="https://www.baidu.com/img/bd_logo1.png">
+                <img src="https://www.baidu.com/img/bd_logo1.png">
+                <img src="https://www.baidu.com/img/bd_logo1.png">
+            </div>
+            <p>火星时报  52跟帖</p>
+        </div>
     </div>
 </template>
 
@@ -55,6 +66,36 @@ export default {
         display: block;
         object-fit: cover;
         margin-left: 10/360*100vw;
+    }
+}
+
+.imgList{
+    padding: 20/360*100vw;
+    border-bottom: 1px #eee solid;
+
+    h4{
+        // 文字超出两行出现省略号
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        overflow: hidden;
+        font-weight: normal;
+    }
+
+    .images{
+        margin: 10/360*100vw 0;
+        display: flex;
+        justify-content: space-between;
+        img{
+            width: 33%;
+            height: 75/360*100vw;
+            object-fit: cover;
+        }
+    }
+
+    p{
+        color: #999;
+        margin-top:5px;
     }
 }
 </style>
