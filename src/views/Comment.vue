45dd3beacdf6f3929f54f3be9f2acd6d0d5d4927
itcast-hsian <407775611@qq.com>
2020-03-30 15:04:56 +0800

    我的跟帖数据的请求和渲染

--- src/views/Comment.vue
+++ src/views/Comment.vue
@@ -4,37 +4,65 @@
         <NavigateBar title="我的跟帖"/>
 
         <!-- 要循环的结构 -->
-        <div class="comment-item">
-            <div class="date">2019-20-20 20:20</div>
+        <div class="comment-item" v-for="(item, index) in comments" :key="index">
+            <div class="date">{{ moment(item.create_date).format("YYYY-MM-DD hh:mm") }}</div>
             <!-- 有回复的评论需要展示的, 就是我回复了谁 -->
-            <div class="parent">
-                <div class="parent-user">回复：火星网友</div>   
+            <div class="parent" v-if="item.parent">
+                <div class="parent-user">回复：{{item.parent.user.nickname}}</div>   
                 <div class="parent-content">
-                    有回复的评论需要展示的, 就是我回复了谁有回复的评论需要展示的, 就是我回复了谁
+                    {{item.parent.content}}
                 </div>
             </div>
 
             <!-- 自己发布的内容 -->
             <div class="comment-content">
-                自己发布的内容自己发布的内容自己发布的内容自己发布的内容自己发布的内容自己发布的内容自己发布的内容自己发布的内容
+                {{item.content}}
             </div>
 
+            <!-- 回复的文章标题 -->
             <router-link to="#" class="link-post">
-                原文：发布评论的新闻文章的标题
+                原文：{{item.post.title}}
+                <span class="iconfont iconjiantou1"></span>
             </router-link>
         </div>
-
     </div>
 </template>
 
 <script>
 // 引入头部导航组件
 import NavigateBar from "@/components/NavigateBar"
+// 日期处理的工具库
+import moment from "moment";
 
 export default {
+    data(){
+        return {
+            // 评论列表
+            comments: [],
+            // 日期处理的工具库
+            moment
+        }
+    },
     // 注册组件
     components: {
         NavigateBar
+    },
+
+    mounted(){
+        // 本地的数据
+        const localUserJson = JSON.parse(localStorage.getItem("userInfo"));
+        // 请求评论列表
+        this.$axios({
+            url: "/user_comments",
+            // 添加头信息
+			headers: {
+				Authorization: localUserJson.token
+			}
+        }).then(res => {
+            const {data} = res.data;
+            // 把评论列表保存到data
+            this.comments = data;
+        })
     }
 };
 </script>
@@ -59,7 +87,20 @@ export default {
     }
 
     .link-post{
+        display: flex;
+        align-items: center;
+        position: relative;
+        padding-right: 30px;
         color:#999;
+        // 文字超出出现省略号一定是块元素才有作用
+        overflow: hidden;
+        text-overflow:ellipsis;
+        white-space: nowrap;
+
+        span{
+            position: absolute;
+            right: 0;
+        }
     }
 }
 </style>
