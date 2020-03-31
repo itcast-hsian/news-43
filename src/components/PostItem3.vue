f2f649986b12d6b7a654e03cb0fb19205323ee34
itcast-hsian <407775611@qq.com>
2020-03-31 14:28:23 +0800

    修改一下视频播放的按钮

--- src/components/PostItem3.vue
+++ src/components/PostItem3.vue
@@ -3,7 +3,7 @@
         <h4>林志玲穿透视黑纱裙米兰看秀腹部微隆显孕味</h4>
         <div class="cover">
             <!-- 封面图片 -->
-            <img src="https://img14.360buyimg.com/pop/s590x470_jfs/t1/110562/14/10737/42247/5e81bb76E6fffa006/b3f703f750bd37ea.jpg.webp">
+            <img src="https://img20.360buyimg.com/da/s590x470_jfs/t1/6412/18/141/70492/5bacdaf3E48cea54f/414c8264705bd268.jpg.webp">
             <!-- 图片上的按钮 -->
             <div class="cover-layer">
                 <span class="iconfont iconshipin"></span>
@@ -54,6 +54,14 @@ export default {
             left: 50%;
             top:50%;
             transform: translateX(-27/360*100vw) translateY(-27/360*100vw);
+            display: flex;
+            align-items:center;
+            justify-content: center;
+
+            .iconshipin{
+                color: #fff;
+                font-size: 32px;;
+            }
         }
     }
 
