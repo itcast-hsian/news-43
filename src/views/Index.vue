45d120ff0527a98c4a0d37372430657b5cfb930b
itcast-hsian <407775611@qq.com>
2020-03-31 10:28:22 +0800

    首页的头部布局

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>首页</div>
+    <div>
+        <!-- 红色的头部 -->
+        <div class="header">
+            <span class="iconfont iconnew"></span>
+            <router-link to="#" class="search">
+                <span class="iconfont iconsearch"></span>
+                <i>搜索新闻</i>
+            </router-link>
+            <router-link to="/personal">
+                <span class="iconfont iconwode"></span>
+            </router-link>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +20,43 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.header{
+    height:50/360*100vw;
+    background: #ff0000;
+    display: flex;
+    padding:0 20/360*100vw;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
 
+    .iconnew{
+        font-size:20px;
+        transform: scale(3);
+        position: relative;
+        left: 20/360*100vw;
+    }
+
+    .search{
+        flex: 1;
+        height: 32/360*100vw;
+        margin: 0 50/360*100vw;
+        background: rgba(255,255,255,0.5);
+        display: flex;
+        justify-content: center;
+        align-items:center;
+        border-radius: 50px;
+        font-size: 13px;
+        line-height: 1;
+        vertical-align: middle;
+
+        span{
+            margin-right: 5px;
+        }
+    }
+
+    .iconwode{
+        font-size: 20px;
+    }
+}
 </style>
