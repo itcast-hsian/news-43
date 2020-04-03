4b06a3626e11b37df006137eaa73ed2178b76f83
itcast-hsian <407775611@qq.com>
2020-04-03 09:33:39 +0800

    初始化首页到前一天的提交，没有写逻辑之前都是静态页面

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -17,39 +17,24 @@
         <!-- sticky：是否使用粘性定位布局 -->
         <!-- swipeable: 是否开启手势滑动切换 -->
         <van-tabs v-model="active" sticky swipeable>
-            <van-tab v-for="(item, index) in categories" :key="index" :title="item.name">
+            <van-tab v-for="(item, index) in categories" :key="index" :title="item">
 
                 <!-- 下拉刷新 -->
                 <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
                     <!-- van的列表组件 -->
                     <!-- @load 滚动到底部时候触发的函数 -->
-                    <!-- immediate-check 这个属性可以阻止list组件默认就加载一次 -->
                     <van-list
-                        :immediate-check="false"
-                        v-model="categories[active].loading"
-                        :finished="categories[active].finished"
+                        v-model="loading"
+                        :finished="finished"
                         finished-text="我也是有底线的"
                         @load="onLoad"
                     >
-                        
+
                         <!-- 假设list是后台返回的数组，里有10个元素 -->
-                        <div v-for="(item, index) in categories[active].posts" :key="index">
+                        <div v-for="(item, index) in list" :key="index">
                             <!-- 只有单张图片的 -->
-                            <PostItem1 
-                            v-if="item.type === 1 && item.cover.length > 0 && item.cover.length < 3"
-                            :data="item"/>
-
-                            <!-- 大于等于3张图片 -->
-                            <PostItem2 
-                            v-if="item.type === 1 && item.cover.length >= 3"
-                            :data="item"/>
-
-                            <!-- 视频 -->
-                            <PostItem3 
-                            v-if="item.type === 2"
-                            :data="item"/>
+                            <PostItem1/> {{index}}
                         </div>
-                    
                     </van-list>
                 </van-pull-refresh>
             </van-tab>
@@ -69,17 +54,14 @@ export default {
     data(){
         return {
             // 菜单的数据
-            // categories: ['关注','娱乐','体育','汽车','房产','关注',
-            // '关注','娱乐','体育','汽车','房产','关注', "∨"],
-            categories: [],
+            categories: ['关注','娱乐','体育','汽车','房产','关注',
+            '关注','娱乐','体育','汽车','房产','关注', "∨"],
             // 记录当前tab的切换的索引
             active: 0,
-            // 记录当前的栏目的id
-            // categoryId: 999,
-            // 假设这个文章数组是后台返回的数据
-            // list: [],
-            //loading: false, // 是否正在加载中
-            //finished: false, // 是否已经加载完毕
+            // 假设这个数组是后台返回的数据
+            list: [1,1,1,1,1,1,1,1,1,1], // 10个1
+            loading: false, // 是否正在加载中
+            finished: false, // 是否已经加载完毕
             refreshing: false , // 是否正在下拉加载
         }
     },
@@ -90,11 +72,7 @@ export default {
             // 判断如果点击的是最后一个图标，跳转到栏目管理页
             if(this.active === this.categories.length - 1){
                 this.$router.push("/栏目管理")
-                return;
             }
-
-            // 请求不同的栏目的数据
-            this.getList();
         }
     },
     components: {
@@ -102,139 +80,25 @@ export default {
         PostItem2,
         PostItem3
     },
-    mounted(){
-        // 在请求之前，应该先判断本地有没栏目数据
-        const categories = JSON.parse(localStorage.getItem("categories"));
-        // 本地的token
-        const {token} = JSON.parse(localStorage.getItem('userInfo')) || {};
-
-        // 如果本地有数据，获取本地的数据来渲染
-        if(categories){
-            // 登录了(有token)但是第一条不是关注
-            if(categories[0].name !== "关注" && token){
-                // 获取栏目数据
-                this.getCategories(token);
-                return;
-            }
-            // 登录了(有token)但是第一条不是关注
-            if(categories[0].name === "关注" && !token){
-                // 获取栏目数据
-                this.getCategories();
-                return;
-            }
-            this.categories = categories;
-
-            // 给每个栏目都加上pageIndex = 1
-            this.handleCategories();
-        }else{
-            // 获取栏目数据
-            this.getCategories(token);
-        }
-
-        // 请求文章列表，页面一开始都是请求头条栏目下的文章，头条栏目的id是999
-        this.$axios({
-            url: "/post",
-            // params就是url问号后面的参数
-            params: {
-                pageIndex: 1,
-                pageSize: 5,
-                category: this.categories[this.active].id
-            }
-        }).then(res => {
-            // 文章的数据
-            const {data} = res.data;
-            // 如果是修改数组中某一项的属性，不会驱动视图的更新的
-            this.categories[this.active].posts = data;
-            // 赋值的方式可以引起模板的刷新
-            this.categories = [...this.categories];
-        });
-    },
     methods: {
-        // 当栏目数据加载完成后
-        // 循环给栏目加上pageIndex, 文章列表, 加载中的状态，是否加载完成
-        handleCategories(){
-            this.categories = this.categories.map(v => {
-                v.pageIndex = 1;
-                v.posts = [];
-                v.loading = false;
-                v.finished = false;
-                return v;
-            })
-        },
-
-        // 获取栏目数据, 如果有token加上到头信息。没有就不加
-        getCategories(token){
-            // 请求的配置
-            const config = {
-                url: "/category"
-            }
-            // 如果有token，把token添加到头信息中
-            // 如果有token请求回来的数据就会有关注的栏目，因为代表是登录的
-            // 如果没有token就没有关注的栏目
-            if(token){
-                config.headers = { Authorization: token };
-            }
-
-            // 没有本地的数据才去请求栏目接口
-            this.$axios(config).then(res => {
-                // 菜单的数据
-                const {data} = res.data;
-                // 给data添加一个点击跳转到栏目管理的图标
-                data.push({
-                    name: "∨"
-                })
-                this.categories = data;
-                // 把菜单的数据保存到本地
-                localStorage.setItem("categories", JSON.stringify(data));
-                // 给每个栏目都加上pageIndex = 1
-                this.handleCategories();
-            })
-        },
-
-        // 加载下一页的数据
         onLoad() {
-            // 当前栏目下pageIndex加1
-            this.categories[this.active].pageIndex += 1
-
-            // 请求文章列表
-            this.getList();
-        },
-
-        // 封装一个请求文章列表的方法
-        getList(){
-            // 如果当前的栏目数据已经加载完毕了，直接return；
-            if(this.categories[this.active].finished){
-                return;
-            }
-            const {pageIndex, id} = this.categories[this.active];
-
-            // 加载下一页的数据
-            this.$axios({
-                url: "/post",
-                params: {
-                    pageIndex: pageIndex,
-                    pageSize: 5,
-                    category: id
+            console.log("已经拖动到了底部")
+            // 异步更新数据
+            // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+            setTimeout(() => {
+                for (let i = 0; i < 10; i++) {
+                    this.list.push(1);
                 }
-            }).then(res => {
-                const {data, total} = res.data;
-                // 把新的文章数据合并到原来的文章列表中
-                this.categories[this.active].posts = [...this.categories[this.active].posts, ...data]; 
 
                 // 加载状态结束
-                this.categories[this.active].loading = false;
+                this.loading = false;
 
-                // 赋值的方式页面才会更新
-                this.categories = [...this.categories];
-
-                // 是否是最后一页
-                if(this.categories[this.active].posts.length === total){
-                    // 当前栏目的文章已经加载完毕
-                    this.categories[this.active].finished = true;
+                // 数据全部加载完成
+                if (this.list.length >= 40) {
+                    this.finished = true;
                 }
-            })
+            }, 5000);
         },
-
         onRefresh() {
             // 表示加载完毕
             this.refreshing = false;
