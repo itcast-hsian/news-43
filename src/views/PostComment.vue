626697b1da8dad326285040cca833646a9e3f906
itcast-hsian <407775611@qq.com>
2020-04-09 10:37:56 +0800

    回复的楼层布局和样式

--- src/views/PostComment.vue
+++ src/views/PostComment.vue
@@ -15,6 +15,52 @@
                 </div>
                 <span class="reply">回复</span>
             </div>
+
+            <!-- 回复的列表 -->
+            <div class="comment-floor">
+                <div class="floor-top">
+                    <div class="user">
+                        <span>1</span>
+                        <em>火星网友</em>
+                        <span>2小时前</span>
+                    </div>
+                    <span class="reply">回复</span>
+                </div>
+                <div class="content">
+                    文章评论的回复
+                </div>
+
+                <!-- 第二级 -->
+                <div class="comment-floor">
+                    <div class="floor-top">
+                        <div class="user">
+                            <span>2</span>
+                            <em>火星网友</em>
+                            <span>2小时前</span>
+                        </div>
+                        <span class="reply">回复</span>
+                    </div>
+                    <div class="content">
+                        文章评论的回复
+                    </div>
+
+                    <!-- 第3级 -->
+                    <div class="comment-floor">
+                        <div class="floor-top">
+                            <div class="user">
+                                <span>3</span>
+                                <em>火星网友</em>
+                                <span>2小时前</span>
+                            </div>
+                            <span class="reply">回复</span>
+                        </div>
+                        <div class="content">
+                            文章评论的回复
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div class="content">
                 文章说的很有道理！
             </div>
@@ -35,35 +81,61 @@ export default {
 
 <style scoped lang="less">
 .comment{
-    padding: 20/360*100vw;
-    border-bottom: 1px #eee solid;
-    .comment-top{
+    padding: 15/360*100vw;
+    border-bottom: 1px #ddd solid;
+    font-size: 13px;
+}
+.comment-top{
+    display: flex;
+    justify-content: space-between;
+    .user{
         display: flex;
-        justify-content: space-between;
-        .user{
-            display: flex;
-            align-items: center;
-            img{
-                display: block;
-                width: 35/360*100vw;
-                height: 35/360*100vw;
-                border-radius: 50%;
-                margin-right: 5px;
-            }
-
-            span{
-                color: #999;
-                font-size: 12px;
-            }
+        align-items: center;
+        img{
+            display: block;
+            width: 35/360*100vw;
+            height: 35/360*100vw;
+            border-radius: 50%;
+            margin-right: 5px;
         }
 
-        .reply{
+        span{
+            color: #999;
             font-size: 12px;
         }
     }
 
+    .reply{
+        font-size: 12px;
+    }
+}
+
+.content{
+    margin-top: 10/360*100vw;
+}
+
+.comment-floor{
+    margin-top: 10/360*100vw;
+    border: 1px #ddd solid;
+    padding: 2px;
+    .floor-top{
+        display: flex;
+        justify-content: space-between;
+        padding: 5px;
+
+        span{
+            font-size: 12px;
+            color: #999;
+            margin-right:5px;
+        }
+        em{
+            margin-right: 5px;
+        }
+    }
+
     .content{
-        margin-top: 10/360*100vw;
+        padding: 5px;
+        padding-top: 0;
     }
 }
 </style>
