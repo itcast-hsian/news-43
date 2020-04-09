b2ff5ee32af4049e1b2058621340810b5c48dc66
itcast-hsian <407775611@qq.com>
2020-04-09 15:27:43 +0800

    添加评论的输入框

--- src/views/PostComment.vue
+++ src/views/PostComment.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="container">
         <!-- 头部导航组件 -->
         <NavigateBar title="精彩跟帖"/>
 
@@ -37,6 +37,19 @@
                 </div>
             </div>
         </van-list>
+
+        <!-- 发布评论的底部 -->
+        <div class="publish">
+            <!-- 输入框，点击和没点击时候显示的效果是不一样的 -->
+            <van-field
+                v-model="message"
+                :rows="rows"
+                autosize
+                type="textarea"
+                placeholder="说点什么..."
+                class="textarea"
+                />
+        </div>
     </div>
 </template>
 
@@ -65,7 +78,11 @@ export default {
             // 请求的页数
             pageIndex: 1,
             // 请求的条数
-            pageSize: 5
+            pageSize: 5,
+            // 发布评论的数据
+            message: "",
+            // 发布评论输入框的行数
+            rows: 1
         }
     },
     components: {
@@ -113,6 +130,9 @@ export default {
 </script>
 
 <style scoped lang="less">
+.container{
+    padding-bottom: 60/360*100vw;
+}
 .comment{
     padding: 30/360*100vw 15/360*100vw;
     border-bottom: 1px #eee solid;
@@ -147,5 +167,21 @@ export default {
 
 .content{
     margin-top: 10/360*100vw;
+}
+
+.publish{
+    position: fixed;
+    width: 100%;
+    left:0;
+    bottom:0;
+    padding: 5/360*100vw 15/360*100vw;
+    box-sizing: border-box;
+    background: #fff;
+
+    .textarea{
+        background: #eee;
+        border-radius: 50px;
+        padding: 5px 20px;
+    }
 }
 </style>
